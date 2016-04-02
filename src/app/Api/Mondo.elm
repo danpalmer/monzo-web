d20c16706bb48f9ca1d6fd49789d80393c7eab70
Dan Palmer <dan@danpalmer.me>
2016-04-02 20:26:38 +0100

    Add exchanging of auth code for token

--- src/app/Api/Mondo.elm
+++ src/app/Api/Mondo.elm
@@ -3,6 +3,8 @@ module Api.Mondo (..) where
 import Erl
 import Dict
 import Http
+import Http.Extra exposing (..)
+import String exposing (..)
 import Task exposing (..)
 import Effects exposing (Effects)
 import Random exposing (generate, Seed)
@@ -12,7 +14,10 @@ import Settings
 import Prelude exposing (..)
 import Storage
 import Json.Encode as JE
-import Json.Decode as JD
+import Json.Decode as JD exposing ((:=))
+
+
+-- Login
 
 
 stateGenerator : Random.Generator String
@@ -37,7 +42,7 @@ loginUrl seed redirectUrl =
     ( { url
         | query =
             Dict.fromList
-              [ "client_id" => "oauthclient_0000968G0rIJ6Uc40n0iHZ"
+              [ "client_id" => mondoClientID
               , "redirect_uri" => Erl.toString redirectUrl
               , "response_type" => "code"
               , "state" => state
@@ -49,7 +54,56 @@ loginUrl seed redirectUrl =
 
 
 
+-- Access Token
+
+
+type alias AuthDetails =
+  { accessToken : String
+  , expiredIn : Int
+  , refreshToken : String
+  , userID : String
+  }
+
+
+exchangeAuthCode : String -> Erl.Url -> Task never (Maybe AuthDetails)
+exchangeAuthCode code redirectUrl =
+  let
+    data =
+      [ "grant_type" => "authorization_code"
+      , "client_id" => mondoClientID
+      , "client_secret" => mondoClientSecret
+      , "redirect_uri" => Erl.toString redirectUrl
+      , "code" => code
+      ]
+  in
+    post "https://api.getmondo.co.uk/oauth2/token"
+      |> withUrlEncodedBody data
+      |> withHeader "Content-type" "application/x-www-form-urlencoded"
+      |> send (jsonReader decodeAuthDetails) stringReader
+      |> Task.map (\response -> response.data)
+      |> Task.toMaybe
+
+
+decodeAuthDetails : JD.Decoder AuthDetails
+decodeAuthDetails =
+  JD.object4
+    AuthDetails
+    ("access_token" := JD.string)
+    ("expires_in" := JD.int)
+    ("refresh_token" := JD.string)
+    ("user_id" := JD.string)
+
+
+
 -- Constants
+
+
+mondoClientID =
+  "oauthclient_0000968G0rIJ6Uc40n0iHZ"
+
+
+mondoClientSecret =
+  "Y/qw1c4pA8+3rHDch58n6Aw7CNj0W1oWS/n2Rkv+CLkCaRjBkeTia7yQ7JrNMeA2wQPcoJ8Y+lDpd5P5RXo6"
 
 
 mondoOAuthStateKey =
