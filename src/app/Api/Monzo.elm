56f14a7c323e4a812008c1b933b60cd99ebc410d
Dan Palmer <dan@danpalmer.me>
2016-09-03 22:03:58 +0100

    Add some of the APIs

--- src/app/Api/Monzo.elm
+++ src/app/Api/Monzo.elm
@@ -1,13 +1,17 @@
 module Api.Monzo exposing (..)
 
+import Date exposing (Date)
 import Dict
 import Task exposing (..)
+import Json.Decode exposing (Decoder)
 import Erl
-import HttpBuilder exposing (..)
+import HttpBuilder as H exposing (..)
 import Settings
 import Prelude exposing (..)
-import Api.Monzo.Models exposing (ApiAuthDetails)
-import Api.Monzo.Decoder exposing (decodeApiAuthDetails)
+import Utils.Auth exposing (AuthDetails)
+import Api.Monzo.Models exposing (..)
+import Api.Monzo.Decoder exposing (..)
+import Api.Monzo.Util exposing (formatDate)
 
 
 loginUrl : String -> Erl.Url -> Erl.Url
@@ -43,3 +47,57 @@ exchangeAuthCode code redirectUrl =
             |> withHeader "Content-type" "application/x-www-form-urlencoded"
             |> send (jsonReader decodeApiAuthDetails) stringReader
             |> Task.map (\response -> response.data)
+
+
+getAccounts : AuthDetails -> Task (Error String) (List Account)
+getAccounts authDetails =
+    monzoGet "https://api.getmondo.co.uk/accounts" authDetails decodeAccountList []
+
+
+getBalance : AuthDetails -> Account -> Task (Error String) Balance
+getBalance authDetails account =
+    monzoGet "https://api.getmondo.co.uk/accounts"
+        authDetails
+        decodeBalance
+        [ "account_id" => account.id
+        ]
+
+
+getRecentTransactions : AuthDetails -> Account -> Task (Error String) (List Transaction)
+getRecentTransactions authDetails account =
+    getTransactions authDetails account Nothing Nothing
+
+
+getTransactions : AuthDetails -> Account -> Maybe Date -> Maybe Date -> Task (Error String) (List Transaction)
+getTransactions authDetails account before since =
+    let
+        beforeParam =
+            case before of
+                Just date ->
+                    [ "before" => (formatDate date) ]
+
+                Nothing ->
+                    []
+
+        sinceParam =
+            case since of
+                Just date ->
+                    [ "since" => (formatDate date) ]
+
+                Nothing ->
+                    []
+    in
+        monzoGet "https://api.getmondo.co.uk/transactions"
+            authDetails
+            decodeTransactionList
+            [ "account_id" => account.id
+            , "expand[]" => "merchant"
+            ]
+
+
+monzoGet : String -> AuthDetails -> Decoder a -> List ( String, String ) -> Task (Error String) a
+monzoGet url authDetails decoder query =
+    get (H.url url query)
+        |> withHeader "Authorization" ("Bearer " ++ authDetails.accessToken)
+        |> send (jsonReader decoder) stringReader
+        |> Task.map (\response -> response.data)
