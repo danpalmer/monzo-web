1bc332ae89a35cd2dc4dcbb5edab8abf2cbc7fc1
Dan Palmer <dan@danpalmer.me>
2016-09-03 16:49:30 +0100

    Minify HTML

--- webpack.config.js
+++ webpack.config.js
@@ -33,8 +33,28 @@ var commonConfig = {
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: './src/index.html'
-        })
+            template: './src/index.html',
+            minify: {
+                collapseBooleanAttributes: true,
+                collapseWhitespace: true,
+                decodeEntities: true,
+                html5: true,
+                minifyCSS: true,
+                minifyJS: true,
+                processConditionalComments: true,
+                removeAttributeQuotes: true,
+                removeComments: true,
+                removeEmptyAttributes: true,
+                removeOptionalTags: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                removeTagWhitespace: true,
+                sortAttributes: true,
+                sortClassName: true,
+                useShortDoctype: true
+            }
+        }),
     ],
 
     postcss: [
