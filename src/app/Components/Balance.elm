f8da6746753e9c270e913a7e0178e41a51566ece
Dan Palmer <dan@danpalmer.me>
2016-09-04 10:38:46 +0100

    Balance component

--- src/app/Components/Balance.elm
+++ src/app/Components/Balance.elm
@@ -2,12 +2,57 @@ module Components.Balance exposing (view)
 
 import Html exposing (..)
 import Html.Attributes exposing (class)
-import Api.Monzo.Models exposing (Account, Balance)
+import Api.Monzo.Models exposing (Account, Balance, Currency(..))
 
 
 view : Account -> Balance -> Html a
 view account balance =
     div [ class "component-balance" ]
-        [ div [ class "balance" ] [ text "£10.00" ]
-        , div [ class "spend-today" ] [ text "£2.50" ]
+        [ div [ class "balance-unit" ]
+            [ viewAmount balance.balance balance.currency
+            , div [ class "description" ] [ text "Card Balance" ]
+            ]
+        , div [ class "balance-unit" ]
+            [ viewAmount balance.spendToday balance.currency
+            , div [ class "description" ] [ text "Spend Today" ]
+            ]
         ]
+
+
+viewAmount : Int -> Currency -> Html a
+viewAmount amount currency =
+    div [ class "balance" ]
+        [ span [ class "currency" ] [ text (formatCurrency currency) ]
+        , span [ class "amount" ] [ text (formatAmount amount) ]
+        ]
+
+
+formatAmount : Int -> String
+formatAmount x =
+    let
+        major =
+            x // 100
+
+        minor =
+            x `rem` 100
+
+        zeros =
+            if minor < 10 then
+                "0"
+            else
+                ""
+    in
+        (toString major) ++ "." ++ zeros ++ (toString minor)
+
+
+formatCurrency : Currency -> String
+formatCurrency c =
+    case c of
+        GBP ->
+            "£"
+
+        USD ->
+            "$"
+
+        EUR ->
+            "€"
